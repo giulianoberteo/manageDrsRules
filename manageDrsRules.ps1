be885bf631d761ea2295751f0be90779cfdf216e
Giuliano Bertello <giuliano.bertello@gmail.com>
2020-10-08 19:46:20 +0100

    added VM Host rules export and disabling

--- manageDrsRules.ps1
+++ manageDrsRules.ps1
@@ -2,20 +2,27 @@ $vcFQDN = "vcenter-3.vcf-s1.vlabs.local"
 $vcUser = "administrator@vsphere.local"
 $vcPassword = "VMware1!"
 $vmAffinityRulesFile = ".\" + $vcFQDN + "_vmAffinityRules.csv"
-$drsVMHostRules = ".\" + $vcFQDN + "_
-drsVMHostRules.csv"
+$drsVMHostRulesFile = ".\" + $vcFQDN + "_drsVMHostRules.csv"
 $clusterName = "s1-cl01"
 
 Connect-VIServer $vcFQDN -username $vcUser -password $vcPassword
 
 $cluster = Get-Cluster -Name $clusterName
 
-# Check for VM affinity/anti-affinity rules
+# Check for DRS VM affinity/anti-affinity rules
 $vmAffinityRules = Get-DrsRule -Cluster $cluster
-
 if ($vmAffinityRules) {
+
     $vmAffinityRules | Export-Csv $vmAffinityRulesFile
     $vmAffinityRules | Set-DrsRule -Enabled $false
 }
 
-#Get-DrsVMHostRule -Cluster $cluster | Set-DrsVMHostRule -Enabled $false
+# Check for DRS VM host rules
+$vmHostRules = Get-DrsVMHostRule -Cluster $cluster
+if ($vmHostRules) {
+
+    $vmHostRules | Export-Csv $drsVMHostRulesFile
+    $vmHostRules | Set-DrsVMHostRule -Enabled $false
+
+}
+
